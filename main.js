f55e1a289d2497649e2d52962856de0fd0c99a98
NivasReddyP <nivasreddy2401@gmail.com>
2023-03-16 19:25:20 +0530

    f

--- main.js
+++ main.js
@@ -2,15 +2,9 @@ document.querySelector('#authorize').addEventListener('click',getAuthorization)
 const AUTHORIZE = "https://accounts.spotify.com/authorize"
 const TOKEN = "https://accounts.spotify.com/api/token";
 
-window.onload = () => {
-    if ( window.location.search.length > 0 ){
-        handleRedirect();
-    }
-}
-
 function getAuthorization(){
-const client_id = "d330537d0c074dad9355cc05e2423014";
-const secret_id = "aa928b3e7c5446619cf2101e99720eb8"; 
+const client_id = "12cd9bc4731d4da6814f1b9f20c2e9cb";
+const secret_id = "1950f593870c432d941914b3dc057627"; 
 const redirect_uri = "http://127.0.0.1:5500/"
 const scope = 'user-read-private user-read-email user-top-read';
 
@@ -20,11 +14,12 @@ url += '&client_id=' + encodeURIComponent(client_id);
 url += '&scope=' + encodeURIComponent(scope);
 url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
 url += '&show_dialog=true';
+console.log(url,"the url is ")
 window.location.href = url
 }
 
 const accessToken = window.location.hash.substr(1).split('&')[0].split('=')[1]
-
+console.log(accessToken)
 var endpointUrl = 'https://api.spotify.com/v1/me/top/tracks';
 
 var headers = {
--- server.js
+++ server.js
@@ -1,60 +1,41 @@
 const express = require('express')
+const axios = require('axios');
 const querystring = require('querystring');
-// const app = express();
 
-// //app.use(express.urlencoded())
-
-// const AUTHORIZE = "https://accounts.spotify.com/authorize"
-// const TOKEN = "https://accounts.spotify.com/api/token";
-
-// const client_id = "d330537d0c074dad9355cc05e2423014";
-// const client_secret = "aa928b3e7c5446619cf2101e99720eb8"; 
-// const redirect_uri = "http://localhost:3000/callback"
-// const scope = 'user-read-private user-read-email user-top-read';
-
-// app.get('/',(req,res)=>{
-//     res.redirect('https://accounts.spotify.com/authorize?' + 
-//     querystring.stringify(
-//     {response_type: 'code',
-//     client_id: client_id,
-//     scope: scope,
-//     redirect_uri: redirect_uri}))
-// });
-
-// app.get('/callback',(req,res) =>{
-//   const code = req.query.code
-//   res.send('hello')
-// })
-
-// app.listen(3000)
-var app = express();
-app.listen(3000);
-
-var client_id = "d330537d0c074dad9355cc05e2423014";
-var redirect_uri = "http://localhost:3000/callback";
+const app = express();
+const PORT = 3000;
 
 
-app.get('/login', function(req, res) {
+const AUTHORIZE = "https://accounts.spotify.com/authorize";
+const TOKEN =     "https://accounts.spotify.com/api/token";
+const client_id = "12cd9bc4731d4da6814f1b9f20c2e9cb";
+const secret_id = "1950f593870c432d941914b3dc057627";  
+const REDIRECT_URI = "http://localhost:3000/callback"
+const SCOPE = 'user-read-private user-read-email user-top-read';
 
-  var scope = 'user-read-private user-read-email user-top-read';
+app.get('/', (req, res) => {
+  res.send(200)
+});
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
+app.get('/login', (req, res) => {
+  res.redirect(AUTHORIZE +
     querystring.stringify({
       response_type: 'code',
       client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri
+      scope: SCOPE,
+      redirect_uri: REDIRECT_URI
     }));
 });
 
+
+
 app.get('/callback',(req,res)=>{
-  
-  var code = req.query.code;
-  console.log(code)
-  res.end()
-  
+
+})
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
-
-
-
-
